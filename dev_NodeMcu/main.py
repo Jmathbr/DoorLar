c1e33db161b314bc254275e4c4c19d2ce701506f
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-04-27 17:20:30 -0300

    ported led to NeoPixel

--- dev_NodeMcu/main.py
+++ dev_NodeMcu/main.py
@@ -4,43 +4,43 @@ import time
 import ujson
 
 import time
+import machine, neopixel
 from machine import Pin
 
 #Defining constants
-LED_OFF = RELAY_OFF = 0
-LED_ON = RELAY_ON = 1
+RELAY_OFF = 0
+RELAY_ON = 1
+NP_OFF = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (128, 128, 128)
 
-#Pinnig state LEDS
-redLed = Pin(12, Pin.OUT) 
-yellowLed = Pin(13, Pin.OUT) 
-greenLed = Pin(15, Pin.OUT)
+
+#Pinnig state NeoPixel
+np = neopixel.NeoPixel(machine.Pin(13)(1))
 #Relay Pin
-relay = Pin(16, Pin.OUT)
+relay = Pin(12, Pin.OUT)
 
 #Led cycle
 def cycleLeds(cycles):
     i = 0 
     while(i < cycles): 
-        redLed.value(LED_OFF)
-        yellowLed.value(LED_OFF)
-        greenLed.value(LED_ON)
+        np[0] = GREEN
+        np.write()
         time.sleep(0.2)
-        redLed.value(LED_OFF)
-        yellowLed.value(LED_ON)
-        greenLed.value(LED_OFF)
+        np[0] = YELLOW
+        np.write()
         time.sleep(0.2)
-        redLed.value(LED_ON)
-        yellowLed.value(LED_OFF)
-        greenLed.value(LED_OFF)
+        np[0] = RED
+        np.write()
         time.sleep(0.2)
         i = i + 1
-    redLed.value(LED_ON)
-    yellowLed.value(LED_ON)
-    greenLed.value(LED_ON)
+    np[0] = WHITE
+    np.write()
     time.sleep(0.2)
-    redLed.value(LED_OFF)
-    yellowLed.value(LED_OFF)
-    greenLed.value(LED_OFF)
+    np[0] = NP_OFF
+    np.write()
 
 #Startup Led
 def startLed():
@@ -48,9 +48,8 @@ def startLed():
 
 #Led configuration for normal mode
 def normalModeOn():
-    greenLed.value(LED_OFF)
-    yellowLed.value(LED_ON)
-    redLed.value(LED_OFF)
+    np[0] = YELLOW
+    np.write()
     print("---------------")
     print("Normal Mode On.")
     print("---------------")
@@ -59,9 +58,8 @@ def normalModeOn():
 #Led indication that program mode is on
 def programModeOn():
     cycleLeds(2)
-    greenLed.value(LED_ON)
-    yellowLed.value(LED_ON)
-    redLed.value(LED_ON)
+    np[0] = WHITE
+    np.write()
     print("---------------")
     print("Program Mode On.")
     print("---------------")
@@ -69,9 +67,8 @@ def programModeOn():
 
 #Acess granted
 def granted(setDelay):
-    greenLed.value(LED_ON)
-    yellowLed.value(LED_OFF)
-    redLed.value(LED_OFF)
+    np[0] = GREEN
+    np.write()
     relay.value(RELAY_ON)
     print("---------------")
     print("Access Granted.")
@@ -80,9 +77,8 @@ def granted(setDelay):
 
 #Access denied
 def denied():
-    greenLed.value(LED_OFF)
-    yellowLed.value(LED_OFF)
-    redLed.value(LED_ON)
+    np[0] = RED
+    np.write()
     relay.value(RELAY_OFF)
     print("---------------")
     print("Access Denied.")
@@ -91,43 +87,46 @@ def denied():
 
 #Write Sucess
 def sucessWrite():
-    greenLed.value(LED_OFF)
-    yellowLed.value(LED_OFF)
-    redLed.value(LED_OFF)
+    np[0] = NP_OFF
+    np.write()
     time.sleep(0.2)
     i = 0
     while(i < 3):
-        greenLed.value(LED_ON)
+        np[0] = GREEN
+        np.write()
         time.sleep(0.2)
-        greenLed.value(LED_OFF)
+        np[0] = NP_OFF
+        np.write()
         time.sleep(0.2)
         i = i + 1 
 
 #Write Failed
 def failedWrite():
-    greenLed.value(LED_OFF)
-    yellowLed.value(LED_OFF)
-    redLed.value(LED_OFF)
+    np[0] = NP_OFF
+    np.write()
     time.sleep(0.2)
     i = 0
     while(i < 3):
-        redLed.value(LED_ON)
+        np[0] = RED
+        np.write()
         time.sleep(0.2)
-        redLed.value(LED_OFF)
+        np[0] = NP_OFF
+        np.write()
         time.sleep(0.2)
         i = i + 1 
 
 #Delete Sucess
 def sucessDelete():
-    greenLed.value(LED_OFF)
-    yellowLed.value(LED_OFF)
-    redLed.value(LED_OFF)
+    np[0] = NP_OFF
+    np.write()
     time.sleep(0.2)
     i = 0
     while(i < 3):
-        yellowLed.value(LED_ON)
+        np[0] = YELLOW
+        np.write()
         time.sleep(0.2)
-        yellowLed.value(LED_OFF)
+        np[0] = NP_OFF
+        np.write()
         time.sleep(0.2)
         i = i + 1 
 
@@ -139,17 +138,5 @@ print(".\n.\n.\n    Access Control v0.1     \n.\n.\n.")
 
 #Main Loop
 while(True):
-    normalModeOn()
-    time.sleep(3)
-    granted(2)
-    time.sleep(1)
-    denied()
-    programModeOn()
-    time.sleep(2)
-    sucessWrite()
-    time.sleep(1)
-    failedWrite()
-    time.sleep(1)
-    sucessDelete()
-    time.sleep(2)
+    
 
