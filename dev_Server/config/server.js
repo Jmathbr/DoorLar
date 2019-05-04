25bd30385b2d95a0df8775ee659eb27b9a1dc634
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-05-04 16:44:35 -0300

    rm node_modules + add nodemon

--- dev_Server/config/server.js
+++ dev_Server/config/server.js
@@ -1,26 +1,28 @@
-var express = require('express');
-var consign = require('consign');
-var expressValidator = require('express-validator')
-var expressSession = require('express-session');
-var bodyParser = require('body-parser');
+var express = require("express");
+var consign = require("consign");
+var expressValidator = require("express-validator");
+var expressSession = require("express-session");
+var bodyParser = require("body-parser");
 var app = express();
-var dotenv = require('dotenv').load()
-app.set('view engine', 'ejs');
-app.set('views','./app/views');
+var dotenv = require("dotenv").load();
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(expressSession({
-    secret: 'asdfqwerzxcv',
+app.use(
+  expressSession({
+    secret: "asdfqwerzxcv",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 consign()
-    .then('app/controllers')
-    .then('app/models')
-    .include('app/routes')
-    .then('config/dbConnection.js')
-    .into(app)   
+  .then("app/controllers")
+  .then("app/models")
+  .include("app/routes")
+  .then("config/dbConnection.js")
+  .into(app);
 
 module.exports = app;
